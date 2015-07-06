13b645ba921db3f872fa843339f8b86ab0089611
David Wayne Baxter <david@davidwaynebaxter.com>
2015-07-06 08:19:36 -0700

    Added HTML Replace and UglifyJS to build file.

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,21 @@ var babel = require("gulp-babel");
 var clean = require("gulp-clean");
 var shell = require('gulp-shell');
 var bower = require('gulp-bower');
+var htmlreplace = require('gulp-html-replace');
+var uglify = require("gulp-uglifyjs");
 
 gulp.task('bower-install', function() {
     return bower()
         .pipe(gulp.dest('lib/'));
+});
+
+gulp.task("html-replace", function() {
+    return gulp.src("./src/index.html")
+        .pipe(htmlreplace({
+            //'css': 'styles.min.css',
+            'js': 'bundle.min.js'
+        }))
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task("browser-sync", function(){
@@ -20,19 +31,22 @@ gulp.task("browser-sync", function(){
 });
 
 gulp.task("watch-files", function(){
-    gulp.watch("src/index.html", ["copy-index"]);
-    gulp.watch("src/**/*.js", ["babel-it"]);
+    gulp.watch("./src/index.html", ["copy-index"]);
+    gulp.watch("./src/**/*.js", ["babel-it"]);
 });
 
-gulp.task("copy-index", function(){
-    gulp.src("src/**/*.html")
+gulp.task("copy-html", function(){
+    return gulp.src("./src/**/*.html")
         .pipe(gulp.dest('./build'))
         .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task("babel-it", function(){
-    return gulp.src("src/**/*.js")
+    return gulp.src("./src/**/*.js")
         .pipe(babel())
+        .pipe(uglify('bundle.min.js', {
+            outSourceMap: true
+        }))
         .pipe(gulp.dest('./build'))
         .pipe(browserSync.reload({stream:true}));
 });
@@ -41,4 +55,4 @@ gulp.task("clean", function(){
     return gulp.src("./build", {read:false}).pipe(clean());
 });
 
-gulp.task("default", ['clean','bower-install', 'copy-index', 'babel-it', 'browser-sync', 'watch-files']);
+gulp.task("default", ['clean','bower-install', 'babel-it', 'html-replace', 'browser-sync', 'watch-files']);
